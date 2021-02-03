0ec91c59b2dfcba5acf94edb006422a6614a1d72
AbdulWajid768 <62804593+AbdulWajid768@users.noreply.github.com>
2021-02-04 00:03:57 +0500

    Adding Project Files

---
+++ ATM_SOFTWARE_VERSION1.0/Driver.cs
@@ -0,0 +1,18 @@
+﻿using ATM_PL;
+using System;
+using System.IO;
+
+namespace ATM_SOFTWARE_VERSION1._0
+{
+    /// <summary>
+    /// Driver Class
+    /// </summary>
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+            Login_PL login = new Login_PL();
+            login.LoginMenu();
+        }
+    }
+}
